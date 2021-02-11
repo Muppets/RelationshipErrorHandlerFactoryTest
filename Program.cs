09cd2996834faa99b9aac54a5b82b49954406f3b
Neil Bostrom <nbostrom@mediafly.com>
2021-02-11 17:05:57 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using System;
+
+namespace RelationshipErrorHandlerFactoryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = WordprocessingDocument.Open("BrokenURI.docx", isEditable: true, new OpenSettings
+            {
+                RelationshipErrorHandlerFactory = _ => new RemoveMalformedHyperlinksRelationshipErrorHandler(),
+            });
+        }
+
+        private sealed class RemoveMalformedHyperlinksRelationshipErrorHandler : RelationshipErrorHandler
+        {
+            // Works
+            //public override string Rewrite(Uri partUri, string id, string uri) => $"https://error{new string('r', uri.Length)}";
+
+            // Fails with "Data at the root level is invalid"
+            public override string Rewrite(Uri partUri, string id, string uri) => $"https://error";
+        }
+    }
+}
